ebe81967981e42bf4d2f6488667e7917959e4b9e
samxielaptop <crimsoncrympty@gmail.com>
2024-12-31 16:45:48 -0800

    organize

---
+++ Python/main.py
@@ -0,0 +1,28 @@
+from multipledispatch import dispatch
+
+class Skibidi:
+    @dispatch(str, float, int)
+    def __init__(self, type: str, speed: float, hp: int):
+        self.type = type
+        self.speed = speed
+        self.hp = hp
+
+    
+
+        
+
+def promptCreation() -> Skibidi:
+    type = input("type of skibidi: ")
+    speed = float(input("speed of skibidi: "))
+    hp = int(input("hp of skibidi: "))
+    skibidi = Skibidi(type, speed, hp)
+
+    return Skibidi(type, speed, hp)
+
+
+if __name__ == "__main__":
+    print("yahallo! construct ur skibidi army!!!!")
+    soldiers = int(input("how many skibidi do u want to construct? "))
+    armyArr: Skibidi = []
+    for i in range(soldiers):
+        armyArr.append(promptCreation())
--- main.py
+++
@@ -1,9 +0,0 @@
-class Skibidi:
-    def __init__(self, type: str, speed: int):
-        self.type = type
-        self.speed = speed
-
-    
-
-demon1: Skibidi = Skibidi("SpeedDemon", 500)
-print(demon1.type)
